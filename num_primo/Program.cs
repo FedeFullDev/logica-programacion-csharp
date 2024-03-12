ba91e5b8c5a84382ea43439fafa953877d6915c4
FedeFullDev <federicopaglieri07@gmail.com>
2024-03-12 00:22:37 -0300

    Ejercicio num_primo finalizado

---
+++ num_primo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num_primo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              * Escribe un programa que se encargue de comprobar si un número es o no primo.
+              * Hecho esto, imprime los números primos entre 1 y 100.
+            */
+
+            int contador;
+
+            for (int x = 1; x <= 100; x++)
+            {
+                contador = 0;
+
+                for (int y = 1; y <= x; y++)
+                {
+                    if (x % y == 0)
+                    {
+                        contador++;
+                    }
+                }
+
+                if (contador == 2)
+                {
+                    Console.WriteLine(x);
+                }else
+                {
+                    Console.WriteLine($"{x} no es primo");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sucesion_fibonacci/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sucesion_fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              * Escribe un programa que imprima los 50 primeros números de la sucesión
+              * de Fibonacci empezando en 0.
+              * - La serie Fibonacci se compone por una sucesión de números en
+              *   la que el siguiente siempre es la suma de los dos anteriores.
+              *   0, 1, 1, 2, 3, 5, 8, 13...
+            */
+
+
+            int x = 0;
+            int y = 1;
+            int z = 0;
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(z);
+                
+                    x = y;
+                    y = z;
+                    z = x + y;
+
+              
+            }
+
+
+            Console.ReadKey();
+
+        }
+
+
+        // usaré una función recursiva
+
+
+      
+    }
+}
